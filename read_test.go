b63e874ff0540f1b2eb8f01bdd87779856e5af25
Michael Woolnough <michael.woolnough@gmail.com>
2025-04-23 18:21:51 +0100

    added tests for Tree.Child

--- read_test.go
+++ read_test.go
@@ -2,6 +2,7 @@ package tree
 
 import (
 	"bytes"
+	"errors"
 	"reflect"
 	"testing"
 )
@@ -83,4 +84,110 @@ func readTree(t Node) node {
 	}
 
 	return n
+}
+
+func TestChild(t *testing.T) {
+	var buf bytes.Buffer
+
+	WriteTree(&buf, &node{
+		children: []node{
+			{
+				name: "A1",
+				data: []byte("123"),
+				children: []node{
+					{
+						name: "B1",
+						data: []byte("456"),
+					},
+					{
+						name: "B2",
+						data: []byte("789"),
+					},
+					{
+						name: "B3",
+						data: []byte("ABC"),
+					},
+				},
+			},
+			{
+				name: "A2",
+				data: []byte("DEF"),
+				children: []node{
+					{
+						name: "B1",
+						data: []byte("GHI"),
+					},
+					{
+						name: "B2",
+						data: []byte("JKL"),
+					},
+				},
+			},
+		},
+		data: []byte("MNO"),
+	})
+
+Loop:
+	for n, test := range [...]struct {
+		key    []string
+		data   []byte
+		errors []error
+	}{
+		{
+			data: []byte("MNO"),
+		},
+		{
+			key:    []string{"A1"},
+			data:   []byte("123"),
+			errors: []error{nil},
+		},
+		{
+			key:    []string{"A1", "B1"},
+			data:   []byte("456"),
+			errors: []error{nil, nil},
+		},
+		{
+			key:    []string{"A1", "B2"},
+			data:   []byte("789"),
+			errors: []error{nil, nil},
+		},
+		{
+			key:    []string{"A2", "B2"},
+			data:   []byte("JKL"),
+			errors: []error{nil, nil},
+		},
+		{
+			key:    []string{"A2", "B3"},
+			data:   []byte("JKL"),
+			errors: []error{nil, ErrNotFound},
+		},
+		{
+			key:    []string{"A2", "B2", "C1"},
+			data:   []byte("JKL"),
+			errors: []error{nil, nil, ErrNotFound},
+		},
+	} {
+		node := OpenAt(bytes.NewReader(buf.Bytes()), int64(buf.Len()))
+
+		for m := range test.key {
+			child, err := node.Child(test.key[m])
+			if !errors.Is(err, test.errors[m]) {
+				t.Errorf("test %d.%d: expecting error %v, got %v", n+1, m+1, test.errors[m], err)
+			}
+
+			if err != nil {
+				continue Loop
+			}
+
+			node = child
+		}
+
+		var data bytes.Buffer
+
+		node.WriteTo(&data)
+
+		if !bytes.Equal(data.Bytes(), test.data) {
+			t.Errorf("test %d: expecting data %q, got %q", n+1, test.data, data.Bytes())
+		}
+	}
 }
