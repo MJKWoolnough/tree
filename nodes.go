34f93561838cb9b416a4f50ca67f4291cb798534
Michael Woolnough <michael.woolnough@gmail.com>
2025-05-04 20:01:56 +0100

    added Branch type

--- nodes.go
+++ nodes.go
@@ -3,6 +3,8 @@ package tree
 import (
 	"io"
 	"iter"
+	"slices"
+	"strings"
 )
 
 type Leaf []byte
@@ -31,4 +33,63 @@ func (Leaf) Child(name string) (Node, error) {
 
 func (Leaf) NumChildren() int {
 	return 0
+}
+
+type nameNode struct {
+	Name string
+	Node
+}
+
+type Branch []nameNode
+
+func (b *Branch) Add(name string, node Node) error {
+	pos, exists := slices.BinarySearchFunc(*b, nameNode{Name: name}, func(a, b nameNode) int {
+		return strings.Compare(a.Name, b.Name)
+	})
+
+	if exists {
+		return DuplicateChildError{name}
+	}
+
+	*b = slices.Insert(*b, pos, nameNode{Name: name, Node: node})
+
+	return nil
+}
+
+func (b Branch) Children() iter.Seq2[string, Node] {
+	return func(yield func(string, Node) bool) {
+		for _, nn := range b {
+			if !yield(nn.Name, nn) {
+				break
+			}
+		}
+	}
+}
+
+func (Branch) WriteTo(_ io.Writer) (int64, error) {
+	return 0, nil
+}
+
+func (b Branch) Child(name string) (Node, error) {
+	pos, exists := slices.BinarySearchFunc(b, nameNode{Name: name}, func(a, b nameNode) int {
+		return strings.Compare(a.Name, b.Name)
+	})
+
+	if !exists {
+		return nil, ChildNotFoundError(name)
+	}
+
+	return b[pos], nil
+}
+
+func (Branch) Data() []byte {
+	return nil
+}
+
+func (Branch) DataLen() int64 {
+	return 0
+}
+
+func (b Branch) NumChildren() int {
+	return len(b)
 }
