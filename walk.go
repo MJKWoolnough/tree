6d49d5ed033de5c0929bea0c23943597523ef13f
Michael Woolnough <michael.woolnough@gmail.com>
2026-08-04 07:34:36 +0100

    added comments

--- walk.go
+++ walk.go
@@ -2,8 +2,26 @@ package tree
 
 import "errors"
 
+// WalkFunc is the type of the function called by Walk to visit each Node.
+//
+// The path argument contains the names of the Nodes leading to the current
+// Node.
+//
+// The returned error controls how the Walk continues.
+//
+// The SkipNode error will prevent walking the children of the current Node.
+//
+// The SkipAll error will cancel the remaining walk, but return a nil error
+// from Walk.
+//
+// Any other error will be returned via the Walk function.
 type WalkFunc func(path []string, n Node) error
 
+// Walk recursively walks the tree hierarchy, calling the supplied function for
+// each Node visited.
+//
+// See the WalkFunc type for information on the arguments and how the returned
+// error is handled.
 func Walk(n Node, fn WalkFunc) error {
 	if err := walk(n, fn, nil); err != SkipAll {
 		return err
@@ -31,6 +49,9 @@ func walk(n Node, fn WalkFunc, path []string) error {
 }
 
 var (
+	// SkipNode tells the Walk to not iterate through the current Nodes children.
 	SkipNode = errors.New("skip this node")
-	SkipAll  = errors.New("stop the walk")
+
+	// SkipAll tells the Walk to stop and return a nil error.
+	SkipAll = errors.New("stop the walk")
 )
